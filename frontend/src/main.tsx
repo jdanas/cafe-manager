0dca3ad335e2eab56b2c9ce206f94d9d2a249c02
Derek <59902137+jdanas@users.noreply.github.com>
2024-11-19 14:33:32 +0800

    fixed queryClient

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
-import CssBaseline from '@mui/material/CssBaseline';
+import { queryClient } from './lib/queryClient';
 import App from './App';
-import queryClient from './queryClient';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <CssBaseline />
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
